7516c050740a24aab3f29f9cc446c69e437a7111
brian <dpi.studio@gmail.com>
2025-03-23 14:47:27 +0800

    feat: add logging and clarify service registration
    - 新增 `Microsoft.Extensions.Logging` 的 using 指令以啟用日誌功能。
    - 添加註解以說明 Worker Service 中的服務註冊行為。
    - 包含註解掉的 `MyWorker` 註冊行，建議用於 Worker Service。
    - 註解掉原有的 `host.Run()`，並解釋 Web App 或 Worker Service 的運行需求。
    - 新增註解說明一次性執行的服務獲取與執行方式。

--- Console8/Program.cs
+++ Console8/Program.cs
@@ -1,6 +1,7 @@
 ﻿using demoNuGetPacakage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace Console8;
@@ -23,10 +24,16 @@ internal class Program
         builder.Services.AddSerilog();
         builder.Services.AddSingleton<MyService>();
 
-        var host = builder.Build();
-        //host.Run();
+        // 如果是 Worker Service，服務會註冊為 Singleton，並且自動啟動
+        // builder.Services.AddHostedService<MyWorker>(); // MyWorker 為 BackgroundService 實作
 
-        // get service and call method
+        // 如果是一次性執行，只要 build() 就可以
+        var host = builder.Build();
+
+        // WebApp 或 Worker Service 還需 Run() 讓服務持續運行
+        //host.Run(); // 或 await host.RunAsync();
+
+        // 一次性執行程式，build() 以後可從 host.Services 取得服務執行
         var myService = host.Services.GetRequiredService <MyService>();
         myService.DoWork();
     }
