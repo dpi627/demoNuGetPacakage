7142be4d0fc0c418d9eabc0fccbc38eaa6d72bb2
brian <dpi.studio@gmail.com>
2025-03-23 10:42:25 +0800

    feat: add logging and hosting support
    - 更新 `Console8.csproj`，新增 `Microsoft.Extensions.Hosting` 和 `Serilog.AspNetCore` 的套件參考。
    - 在 `Program.cs` 中，移動命名空間聲明並新增使用指令。
    - 修改 `Main` 方法以設置 Serilog 日誌，移除原有的 "Hello, World!" 輸出。
    - 新增應用啟動的日誌條目，並引入 `Host.CreateApplicationBuilder` 以建立應用主機。

--- Console8/Program.cs
+++ Console8/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Console8;
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Console8;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // create serilog
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Starting up");
+
+        var builder = Host.CreateApplicationBuilder(args);
+
     }
 }
