e3f4eece0a6c5d14c5c128bb4e045550f0a8f733
brian <dpi.studio@gmail.com>
2025-03-23 10:22:21 +0800

    feat: add MyLoggerFactory for logger creation
    - 新增 `MyLoggerFactory` 類別於 `demoNuGetPacakage` 命名空間。
    - 提供靜態方法 `CreateLogger<T>` 以創建 `ILogger<T>` 實例。
    - 若 `loggerFactory` 為 null，則返回 `NullLogger<T>` 的單例實例。
    - 添加必要的 using 指令以支援日誌功能。

---
+++ demoNuGetPacakage/MyLoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace demoNuGetPacakage;
+
+/// <summary>
+/// Factory class for creating loggers.
+/// </summary>
+internal class MyLoggerFactory
+{
+    /// <summary>
+    /// Creates a logger for the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type for which to create a logger.</typeparam>
+    /// <param name="loggerFactory">The logger factory to use. If null, a <see cref="NullLogger{T}"/> instance is returned.</param>
+    /// <returns>An <see cref="ILogger{T}"/> instance.</returns>
+    public static ILogger<T> CreateLogger<T>(ILoggerFactory? loggerFactory = default) =>
+        loggerFactory?.CreateLogger<T>() ?? NullLogger<T>.Instance;
+}
