2cd432a7a2755925b612788c1bf1becab70b447e
brian <dpi.studio@gmail.com>
2025-03-25 20:14:25 +0800

    feat: 完成 Builder Mode 範例
    - 更新程式碼以整合 Serilog 進行日誌記錄。
    - 使用 Microsoft.Extensions.DependencyInjection 和 Microsoft.Extensions.Hosting 庫。
    - 修改 `Main` 方法以設置主機生成器並註冊服務。
    - 新增 `MyTest` 類別，展示如何使用依賴注入。

--- Console8BuilderTest/Program.cs
+++ Console8BuilderTest/Program.cs
@@ -1,10 +1,76 @@
-﻿namespace Console8BuilderTest
+﻿using demoNuGetPacakage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Console8BuilderTest;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // create serilog
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Starting up");
+
+        var builder = Host.CreateApplicationBuilder(args);
+
+        // add services
+        builder.Services.AddSerilog();
+
+        // 註冊 IMyServiceBuilder
+        // 進行實例化，並從 ServiceProvider 取得 ILoggerFactory 注入
+        builder.Services.AddScoped<IMyServiceBuilder>(serviceProvider =>
         {
-            Console.WriteLine("Hello, World!");
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            return MyServiceBuilder.Init(loggerFactory);
+        });
+
+        // 註冊 IMyService
+        // 從 ServiceProvider 取得 IMyServiceBuilder 進行實例化
+        // 如果不需要設定參數，可以直接註冊 MyService 或在其他類別取用 IMyServiceBuilder 建立
+        builder.Services.AddScoped<IMyService>(serviceProvider =>
+        {
+            var builderService = serviceProvider.GetRequiredService<IMyServiceBuilder>();
+            return builderService.SetParam1("default value")
+                                 .SetParam2(123)
+                                 .Build();
+        });
+
+        builder.Services.AddTransient<MyTest>();
+
+        // 如果是一次性執行，只要 build() 就可以
+        var host = builder.Build();
+
+        // 一次性執行程式，build() 之後可從 host.Services 取得服務執行
+        var myService = host.Services.GetRequiredService<IMyService>();
+        myService.DoWork();
+
+        var myTest = host.Services.GetRequiredService<MyTest>();
+        myTest.Run();
+
+        // 使用 IMyServiceBuilder 建立 IMyService
+        var serviceBuilder = host.Services.GetRequiredService<IMyServiceBuilder>();
+        myTest.RunWithBuilder(serviceBuilder);
+    }
+
+    class MyTest(IMyService service)
+    {
+        public void Run()
+        {
+            // 使用 constructor 注入的 IMyService
+            service.SetParam1("Woo").SetParam2(500).DoWork();
+        }
+
+        public void RunWithBuilder(IMyServiceBuilder builder)
+        {
+            // 使用 IMyServiceBuilder 建立 IMyService
+            var service = builder.SetParam1("From Builder").SetParam2(888).Build();
+            service.DoWork();
         }
     }
 }
