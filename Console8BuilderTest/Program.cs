c774b9058bd4748e130c566343eab571900e4a1f
brian <dpi.studio@gmail.com>
2025-03-25 19:38:18 +0800

    feat: add Console8BuilderTest project
    - 更新解決方案檔案 `demoNuGetPacakage.sln`，新增 `Console8BuilderTest` 專案。
    - 修改 `Global` 區段以包含新專案的建置配置。
    - 創建 `Console8BuilderTest.csproj`，使用 .NET SDK 並設定相關屬性。
    - 新增 `Program.cs`，包含簡單的 "Hello, World!" 程式。

---
+++ Console8BuilderTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console8BuilderTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
