ccfc443de3eec3a3f52219542a487d6112793a45
brian <dpi.studio@gmail.com>
2025-03-25 19:34:56 +0800

    refactor(MyServiceBuilder): 實作IMyServiceBuilder 方法
    - 修改 `MyServiceBuilder` 類別以實作 `IMyServiceBuilder` 介面，增強建構者模式的靈活性與可擴展性。
    - 將 `_service` 欄位的類型從 `MyService` 變更為 `IMyService`，促進更好的抽象與解耦。
    - 將 `SetParam1` 和 `SetParam2` 方法的回傳類型從 `MyServiceBuilder` 變更為 `IMyServiceBuilder`，以實現一致的介面並支援方法鏈接。
    - 將 `Build` 方法的回傳類型從 `MyService` 更新為 `IMyService`，確保方法遵循介面的合約。

--- demoNuGetPacakage/MyServiceBuilder.cs
+++ demoNuGetPacakage/MyServiceBuilder.cs
@@ -4,9 +4,9 @@ namespace demoNuGetPacakage;
 
 //Factory Pattern：透過 Init 和 Build 控制 MyService 的創建
 //Builder Pattern：透過 SetParam1 和 SetParam2 逐步配置物件
-public class MyServiceBuilder
+public class MyServiceBuilder : IMyServiceBuilder
 {
-    private readonly MyService _service;
+    private readonly IMyService _service;
 
     // 靜態 Init 方法提高了封裝性，MyServiceBuilder() 建構式是 private，外部無法直接創建
     // 透過 Init 方法，可以控制 MyService 的初始化，也可命名為 Create() 或 New()
@@ -16,17 +16,17 @@ public class MyServiceBuilder
     {
         _service = MyService.Init(loggerFactory);
     }
-    public MyServiceBuilder SetParam1(string p1)
+    public IMyServiceBuilder SetParam1(string p1)
     {
         _service.SetParam1(p1);
         return this;
     }
-    public MyServiceBuilder SetParam2(int p2)
+    public IMyServiceBuilder SetParam2(int p2)
     {
         _service.SetParam2(p2);
         return this;
     }
 
     // Build 方法返回 MyService 實例，可在此檢查參數設定
-    public MyService Build() => _service;
+    public IMyService Build() => _service;
 }
