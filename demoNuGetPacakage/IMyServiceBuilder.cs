e2780b09de4f9a1036fc7000b1b8b9d867b38c01
brian <dpi.studio@gmail.com>
2025-03-25 17:47:29 +0800

    feat: 新增 MyServiceBuilder 與介面
    - 新增 `IMyServiceBuilder` 介面，定義設定參數和建立 `IMyService` 實例的方法。
    - 實作 `MyServiceBuilder` 類別，遵循工廠和建造者設計模式。
    - 提供靜態 `Init` 方法以創建 `MyServiceBuilder` 實例，並使用 `ILoggerFactory` 進行日誌記錄。

---
+++ demoNuGetPacakage/IMyServiceBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace demoNuGetPacakage;
+
+public interface IMyServiceBuilder
+{
+    IMyServiceBuilder SetParam1(string p1);
+    IMyServiceBuilder SetParam2(int p2);
+    IMyService Build();
+}
