983c9ef1ba71db0b8ae8613bcd3a954a2038fb31
brian <dpi.studio@gmail.com>
2025-03-29 09:24:50 +0800

    feat: 新增 ServiceCollection 延伸方法
    - 新增靜態類別 `ServiceCollectionExtension`。
    - 提供擴展方法 `UseMyService` 用於註冊 `MyService` 相關服務。
    - 根據目標框架條件編譯，支援 .NET 6 和 .NET 8。
    - 提供過時方法以處理不支援的情況。

---
+++ demoNuGetPacakage/ServiceCollectionExtension.cs
@@ -0,0 +1,46 @@
+﻿#if NET6_0 || NET8_0
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace demoNuGetPacakage;
+
+/// <summary>
+/// 提供 IServiceCollection 的擴展方法。
+/// </summary>
+public static class ServiceCollectionExtension
+{
+    /// <summary>
+    /// 向 IServiceCollection 添加 MyService 相關的服務。
+    /// </summary>
+    /// <param name="collection">IServiceCollection 的實例。</param>
+    public static void UseMyService(this IServiceCollection collection)
+    {
+        collection.AddScoped<IMyServiceBuilder>(serviceProvider =>
+        {
+            // 從 ServiceProvider 取得 ILoggerFactory 注入
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            return MyServiceBuilder.Init(loggerFactory);
+        });
+
+        collection.AddScoped<IMyService>(serviceProvider =>
+        {
+            // 從 ServiceProvider 取得 IMyServiceBuilder 進行實例化
+            var builder = serviceProvider.GetRequiredService<IMyServiceBuilder>();
+            return builder.Build();
+        });
+    }
+}
+#else
+using System;
+
+namespace demoNuGetPacakage;
+
+public static class ServiceCollectionExtension
+{
+    [Obsolete("UseMyService is only available in .NET 6+")]
+    public static void UseMyService(this object collection)
+    {
+        throw new PlatformNotSupportedException("This method is only supported in .NET 6 or later.");
+    }
+}
+#endif
