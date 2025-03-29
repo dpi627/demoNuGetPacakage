272e079206383d67e4c0ca5b072883436e82a0d6
brian <dpi.studio@gmail.com>
2025-03-29 09:25:32 +0800

    refactor: 修改為延伸方法範例
    - 將 `IMyServiceBuilder` 和 `IMyService` 的註冊程式碼註解掉。
    - 使用擴充方法 `UseMyService()` 簡化服務註冊。
    - 提高程式碼可讀性和可維護性。

--- Console8BuilderTest/Program.cs
+++ Console8BuilderTest/Program.cs
@@ -22,24 +22,27 @@ internal class Program
         // add services
         builder.Services.AddSerilog();
 
-        // 註冊 IMyServiceBuilder
-        // 進行實例化，並從 ServiceProvider 取得 ILoggerFactory 注入
-        builder.Services.AddScoped<IMyServiceBuilder>(serviceProvider =>
-        {
-            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-            return MyServiceBuilder.Init(loggerFactory);
-        });
+        //// 註冊 IMyServiceBuilder
+        //// 進行實例化，並從 ServiceProvider 取得 ILoggerFactory 注入
+        //builder.Services.AddScoped<IMyServiceBuilder>(serviceProvider =>
+        //{
+        //    var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        //    return MyServiceBuilder.Init(loggerFactory);
+        //});
 
-        // 註冊 IMyService
-        // 從 ServiceProvider 取得 IMyServiceBuilder 進行實例化
-        // 如果不需要設定參數，可以直接註冊 MyService 或在其他類別取用 IMyServiceBuilder 建立
-        builder.Services.AddScoped<IMyService>(serviceProvider =>
-        {
-            var builderService = serviceProvider.GetRequiredService<IMyServiceBuilder>();
-            return builderService.SetParam1("default value")
-                                 .SetParam2(123)
-                                 .Build();
-        });
+        //// 註冊 IMyService
+        //// 從 ServiceProvider 取得 IMyServiceBuilder 進行實例化
+        //// 如果不需要設定參數，可以直接註冊 MyService 或在其他類別取用 IMyServiceBuilder 建立
+        //builder.Services.AddScoped<IMyService>(serviceProvider =>
+        //{
+        //    var builderService = serviceProvider.GetRequiredService<IMyServiceBuilder>();
+        //    return builderService.SetParam1("default value")
+        //                         .SetParam2(123)
+        //                         .Build();
+        //});
+
+        // 使用擴充方法簡化註冊服務
+        builder.Services.UseMyService();
 
         builder.Services.AddTransient<MyTest>();
 
