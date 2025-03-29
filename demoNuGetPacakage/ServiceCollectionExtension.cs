ddfa8d266d0c968488834437f0e2ca57d9f5cc1a
brian <dpi.studio@gmail.com>
2025-03-29 09:58:35 +0800

    feat: 新增 AddService<T> 方法以註冊服務
    - 新增 `AddService<T>` 方法，支援不同生命週期的服務註冊。
    - 替換對 `IMyServiceBuilder` 和 `IMyService` 的直接 `AddScoped` 調用。
    - 修改 XML 文件註解以增強清晰度。
    - 確保 `UseMyService` 方法僅在 .NET 6 或更高版本可用。

--- demoNuGetPacakage/ServiceCollectionExtension.cs
+++ demoNuGetPacakage/ServiceCollectionExtension.cs
@@ -1,37 +1,57 @@
-﻿#if NET6_0 || NET8_0
+﻿using System;
+
+#if NET6_0 || NET8_0
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace demoNuGetPacakage;
 
-/// <summary>
-/// 提供 IServiceCollection 的擴展方法。
-/// </summary>
 public static class ServiceCollectionExtension
 {
     /// <summary>
-    /// 向 IServiceCollection 添加 MyService 相關的服務。
+    /// 於 IServiceCollection 註冊 MyService 服務。
     /// </summary>
-    /// <param name="collection">IServiceCollection 的實例。</param>
-    public static void UseMyService(this IServiceCollection collection)
+    /// <param name="collection">IServiceCollection 實例</param>
+    public static void UseMyService(
+        this IServiceCollection collection,
+        ServiceLifetime lifetime = ServiceLifetime.Scoped)
     {
-        collection.AddScoped<IMyServiceBuilder>(serviceProvider =>
+        collection.AddService<IMyServiceBuilder>(lifetime, serviceProvider =>
         {
             // 從 ServiceProvider 取得 ILoggerFactory 注入
             var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
             return MyServiceBuilder.Init(loggerFactory);
         });
 
-        collection.AddScoped<IMyService>(serviceProvider =>
+        collection.AddService<IMyService>(lifetime, serviceProvider =>
         {
             // 從 ServiceProvider 取得 IMyServiceBuilder 進行實例化
             var builder = serviceProvider.GetRequiredService<IMyServiceBuilder>();
             return builder.Build();
         });
     }
+
+    private static void AddService<T>(
+        this IServiceCollection collection,
+        ServiceLifetime lifetime,
+        Func<IServiceProvider, T> factory) where T : class
+    {
+        switch (lifetime)
+        {
+            case ServiceLifetime.Singleton:
+                collection.AddSingleton(factory);
+                break;
+            case ServiceLifetime.Scoped:
+                collection.AddScoped(factory);
+                break;
+            case ServiceLifetime.Transient:
+                collection.AddTransient(factory);
+                break;
+        }
+    }
 }
+
 #else
-using System;
 
 namespace demoNuGetPacakage;
 
@@ -43,4 +63,5 @@ public static class ServiceCollectionExtension
         throw new PlatformNotSupportedException("This method is only supported in .NET 6 or later.");
     }
 }
+
 #endif
